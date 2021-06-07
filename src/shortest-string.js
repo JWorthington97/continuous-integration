d4539182e6bb77e342646f933de3f7bfe01059b9
JWorthington97 <jb.worthington@live.co.uk>
2021-06-07 13:51:24 +0100

    Prettier format

--- src/shortest-string.js
+++ src/shortest-string.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 /**
  * Find the shortest string in an array.
@@ -7,14 +7,16 @@
  * @param {string[]} stringArr - array of strings
  * @returns {string} - the shortest string
  */
- function findShortestString(
-    stringArr
-    )
-    { const shortestString = stringArr[0]
-        for (let str of stringArr) {
-    if (str.length < shortestString.length) { shortestString = str
-        }}  return shortestString.length }
-    
-    console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
-    console.log(findShortestString(["why", "hello", "to", "you"]), "to");
-    console.log(findShortestString(["brave", "dance"]), "brave");
+function findShortestString(stringArr) {
+  const shortestString = stringArr[0];
+  for (let str of stringArr) {
+    if (str.length < shortestString.length) {
+      shortestString = str;
+    }
+  }
+  return shortestString.length;
+}
+
+console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
+console.log(findShortestString(["why", "hello", "to", "you"]), "to");
+console.log(findShortestString(["brave", "dance"]), "brave");
